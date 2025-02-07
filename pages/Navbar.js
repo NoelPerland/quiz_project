7bfaf6396bb17b8eb7c6684a25058d3fd4e34369
RobinBroberg <robin.broberg86@gmail.com>
2025-02-07 15:37:25 +0100

    hotfix

--- pages/Navbar.js
+++ pages/Navbar.js
@@ -2,7 +2,7 @@ import { RiAdminFill } from "react-icons/ri";
 
 export default function Navbar() {
   return (
-    <nav className="w-full bg-base-100 shadow-md py-4 px-6 flex justify-between items-center fixed top-0 left-0 right-0">
+    <nav className="w-full bg-base-100 shadow-md py-4 px-6 flex justify-between items-center fixed top-0 left-0 right-0 z-50">
       <h2>
         <a href="/" className="text-2xl font-bold">
           Quix
