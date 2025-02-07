47cd9dee3f5a65d536415f68b658e4f5d5de2482
Karolina Rad <karolina.rad@hotmail.com>
2025-02-07 13:40:37 +0100

    logga

--- pages/Navbar.js
+++ pages/Navbar.js
@@ -1,24 +1,22 @@
 import { RiAdminFill } from "react-icons/ri";
 
 export default function Navbar() {
+
   return (
-    <nav className="navbar bg-gray-900 text-white shadow-md fixed top-0 left-0 right-0 px-6">
-      <div className="navbar-start">
-        <h2 className="text-2xl font-bold">Quix</h2>
-      </div>
-      <div className="navbar-end space-x-6">
-        <a href="/" className="btn btn-ghost text-white hover:bg-gray-700">
+    <nav className="w-full bg-white shadow-md py-4 px-6 flex justify-between items-center fixed top-0 left-0 right-0">
+      <h2><a href="/" className="text-2xl font-bold text-blue-600">Quix</a></h2>
+
+      <div className="flex items-center space-x-6">
+        <a href="/" className="text-gray-700 hover:text-blue-600 transition">
           Home
         </a>
-        <a href="/quiz" className="btn btn-ghost text-white hover:bg-gray-700">
+        <a href="/quiz" className="text-gray-700 hover:text-blue-600 transition">
           Quizzes
         </a>
-        <a
-          href="/admin"
-          className="btn btn-ghost flex items-center text-white hover:bg-gray-700"
-        >
+        <a href="./admin" className="text-gray-700 hover:text-blue-600 transition flex items-center">
           <RiAdminFill className="w-6 h-6" />
         </a>
+
       </div>
     </nav>
   );
